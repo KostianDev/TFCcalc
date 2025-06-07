d306fc32fee117b7cba825966d32d689ce185b1a
KostianDev <verbytskyi.kostiantyn@lll.kpi.ua>
2025-06-07 17:20:02 +0300

    Change port to less popular to prevent cross-usage

--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"fmt"
 	"log"
-	"tfccalc/data"
-	"tfccalc/ui"
 
 	"fyne.io/fyne/v2/app"
+
+	"tfccalc/data"
+	"tfccalc/ui"
 )
 
 func main() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true&charset=utf8mb4",
-		"tfccalc_user", "tfccalc_pass", "127.0.0.1", 3306, "tfccalc_db",
+		"tfccalc_user", "tfccalc_pass", "127.0.0.1", 3405, "tfccalc_db",
 	)
 	if err := data.InitDB(dsn); err != nil {
 		log.Fatalf("Failed to initialize DB: %v", err)
